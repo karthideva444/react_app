fbfad99911b9c033c13178a5265568b5f610d4e1
karthideva444 <ksrthideva444@gmail.com>
2024-04-17 19:35:23 +0530

    useeffect is used with condition

--- demo/src/components/HookCounterOne.js
+++ demo/src/components/HookCounterOne.js
@@ -4,14 +4,17 @@ import { useState,useEffect } from'react'
 function HookCounterOne() {
 
   const [count,setcount] = useState(0)
+  const [Name,setName] = useState('')
 
   useEffect (() => {
+    console.log("updating");
     document.title = `you clicked ${count} times`
-  })
+  },[count])
 
 
   return (
     <div>
+      <input type="text" value={Name} onChange={(e)=> {setName(e.target.value)}} />
       <button onClick={()=>setcount(count + 1)}>click {count} times</button>
     </div>
   )
